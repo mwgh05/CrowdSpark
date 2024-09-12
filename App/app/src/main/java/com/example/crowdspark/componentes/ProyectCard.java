3a1eefe2521c325ba75ce2aa232c9c727ee8a1db
mwgh05 <melwongh@gmail.com>
2024-09-11 21:49:03 -0600

    ventanas

---
+++ App/app/src/main/java/com/example/crowdspark/componentes/ProyectCard.java
@@ -0,0 +1,89 @@
+package com.example.crowdspark.componentes;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.crowdspark.R;
+
+public class ProyectCard extends AppCompatActivity {
+    private String imgurl;
+    private String nombre;
+    private String fecha;
+    private String descripcion;
+    private String monto;
+    private String objetivo;
+
+    public ProyectCard(String imgurl, String nombre, String fecha, String descripcion, String monto, String objetivo) {
+        this.imgurl = imgurl;
+        this.nombre = nombre;
+        this.fecha = fecha;
+        this.descripcion = descripcion;
+        this.monto = monto;
+        this.objetivo = objetivo;
+    }
+
+    public String getImgurl() {
+        return imgurl;
+    }
+
+    public void setImgurl(String imgurl) {
+        this.imgurl = imgurl;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getMonto() {
+        return monto;
+    }
+
+    public void setMonto(String monto) {
+        this.monto = monto;
+    }
+
+    public String getObjetivo() {
+        return objetivo;
+    }
+
+    public void setObjetivo(String objetivo) {
+        this.objetivo = objetivo;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_proyect_card);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ App/app/src/main/java/com/example/crowdspark/ventanas/Principal.java
@@ -0,0 +1,67 @@
+package com.example.crowdspark.ventanas;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.crowdspark.MainActivity;
+import com.example.crowdspark.R;
+import com.example.crowdspark.componentes.ProyectCard;
+import com.example.crowdspark.componentes.ProyectCardAdapter;
+
+import java.util.ArrayList;
+
+public class Principal extends AppCompatActivity {
+
+    private ListView listview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+        listview = (ListView) findViewById(R.id.listview);
+
+        ArrayList <ProyectCard> proyects = new ArrayList<>();
+        proyects.add(new ProyectCard("drawable://" + R.drawable.logo, "Proyecto 1", "10/09/2024", "Descripcion", "100", "1000"));
+        proyects.add(new ProyectCard("drawable://" + R.drawable.logo, "Proyecto 2", "20/10/2024", "Descripcion", "500", "16000"));
+
+        ProyectCardAdapter adapter = new ProyectCardAdapter(this, R.layout.activity_proyect_card, proyects);
+        listview.setAdapter(adapter);
+
+        Button botonCrearProyecto = findViewById(R.id.crearproyecto);
+        botonCrearProyecto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Principal.this, Formulario.class);
+                startActivity(intent);
+            }
+        });
+
+        Button botonEditarProyecto = findViewById(R.id.editarproyecto);
+        botonEditarProyecto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Principal.this, EditarProyecto.class);
+                startActivity(intent);
+            }
+        });
+
+        Button botonEditarUsuario = findViewById(R.id.editarusuario);
+        botonEditarUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Principal.this, EditarUsuario.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
