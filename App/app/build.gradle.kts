e69dab021655753ef70a94b8b01c6caf79f444fd
Emmanuel MJ <emmajose1703@gmail.com>
2024-09-15 13:39:33 -0600

    conectando db
    falta sync con graddle

--- App/app/build.gradle.kts
+++ App/app/build.gradle.kts
@@ -1,5 +1,10 @@
 plugins {
     alias(libs.plugins.androidApplication)
+
+    id("com.android.application")
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -35,6 +40,16 @@ android {
 }
 
 dependencies {
+
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
 
     implementation(libs.appcompat)
     implementation(libs.material)
